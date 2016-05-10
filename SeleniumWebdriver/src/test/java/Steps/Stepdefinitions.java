ad216389386f8ab1a4e32fa9027bbc0f119592b8
Sunia-Imran <sunia.imran@ordnancesurvey.co.uk>
2016-05-10 09:56:29 +0100

    update

--- SeleniumWebdriver/src/test/java/Steps/Stepdefinitions.java
+++ SeleniumWebdriver/src/test/java/Steps/Stepdefinitions.java
@@ -12,46 +12,48 @@ import pageobjects.baseClass;
 import pageobjects.LoginPage;
 
 public class Stepdefinitions extends baseClass {
-	
-	LoginPage loginPage=PageFactory.initElements(baseClass.getdriver(), LoginPage.class);
-		// getting driver from the base class and all the elements from the login page and storing them into the login page variable .
+
+	LoginPage loginPage = PageFactory.initElements(baseClass.getdriver(), LoginPage.class);
+	// getting driver from the base class and all the elements from the login
+	// page and storing them into the login page variable .
 	// now login page will have all the methods on the page
-	
-	
+
 	@Given("^I login to the gmail\"([^\"]*)\"$")
 	public void i_login_to_the_gmail(String Url) throws Throwable {
-		
+
 		loginPage.NavigateToUrl(Url);
-		
+
 	}
 
 	@When("^I enter the email address as \"([^\"]*)\" and \"([^\"]*)\"$")
 	public void i_enter_the_email_address_as_and(String email, String password) throws Throwable {
-		loginPage.Enter_EmailPassword(email,password);
+		loginPage.Enter_EmailPassword(email, password);
 	}
 
 	@When("^i click on inbox$")
 	public void i_click_on_inbox() throws Throwable {
-		
+		loginPage.ClickOnInbox();
 	}
 
 	@When("^I logout$")
 	public void i_logout() throws Throwable {
-
+		loginPage.SignOut();
 	}
 
 	@When("^i check the total numbers of email in the inbox$")
 	public void i_check_the_total_numbers_of_email_in_the_inbox() throws Throwable {
-
-	}
-
-	@When("^I click on star$")
-	public void i_click_on_star() throws Throwable {
-
+		loginPage.TotalNumberOfEmails();
 	}
 
 	@When("^i click on starred emails$")
 	public void i_click_on_starred_emails() throws Throwable {
+		loginPage.clickOnstarredemails();
+	}
+
+	@When("^I click on star$")
+	public void i_click_on_star() throws Throwable {
+		// click on inbox and then star one email
+		loginPage.ClickOnStarInEmais();
 
 	}
 
@@ -62,13 +64,12 @@ public class Stepdefinitions extends baseClass {
 
 	@When("^I click on stared emails$")
 	public void i_click_on_stared_emails() throws Throwable {
-
+		loginPage.clickOnstarredemails();
 	}
-
-
 
 	@When("^I should have the message present as \"([^\"]*)\"$")
 	public void i_should_have_the_message_present_as(String arg1) throws Throwable {
+		loginPage.NoStarredEmails();
 
 	}
 
--- SeleniumWebdriver/src/test/java/pageobjects/LoginPage.java
+++ SeleniumWebdriver/src/test/java/pageobjects/LoginPage.java
@@ -3,6 +3,7 @@ package pageobjects;
 import java.io.File;
 
 import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -11,11 +12,9 @@ import org.openqa.selenium.remote.ScreenshotException;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
-
-
 public class LoginPage extends baseClass {
 
-	@FindBy(how = How.XPATH, using = ".//*[@id='Email']")
+	@FindBy(how = How.XPATH, using = "//*[@id='Email']")
 	public static WebElement Enter_EmailID;
 
 	@FindBy(how = How.XPATH, using = "//*[@id='next']")
@@ -26,7 +25,30 @@ public class LoginPage extends baseClass {
 
 	@FindBy(how = How.XPATH, using = ".//*[@id='signIn']")
 	public static WebElement clicksignIn_Button;
+	/*
+	 * @FindBy(how = How.XPATH, using =
+	 * "//a[@href='https://mail.google.com/mail/u/0/#inbox']") public static
+	 * WebElement Click_Inbox;
+	 */
+	@FindBy(how = How.XPATH, using = "//a[contains(@title,'Inbox (')]")
+	public static WebElement Click_Inbox;
+	@FindBy(how = How.XPATH, using = "//*[@ class='gb_2a gbii']")
+	public static WebElement Signout_link;
+	@FindBy(how = How.XPATH, using = "//*[@id='gb_71'][contains(.,'Sign out')]")
+	public static WebElement signout_button;
 
+	@FindBy(how = How.XPATH, using = ".//*[@href='https://mail.google.com/mail/u/0/#starred']")
+	public static WebElement clickOn_StarredEmails;
+	// Two elements were found with the same xpath value. We captured the
+	// absolute xpath by going into the parent path
+	// we can not capture id's and classes because they change each time
+	@FindBy(how = How.XPATH, using = ".//div[@role='main']//tr[1]/td[3]/span[@title='Not starred']")
+	public static WebElement clickon_Star;
+	@FindBy(how = How.XPATH, using = "//.//tr[1][contains(.,'No starred messages. Stars let you give messages a special status to make them easier to find. To star a message, click on the star outline beside any message or conversation')]")
+	public static WebElement VerifyNoStarredEmails;
+	
+	
+	
 	public void NavigateToUrl(String Url) throws InterruptedException {
 		baseClass.getdriver();
 		driver.get(Url);
@@ -42,27 +64,63 @@ public class LoginPage extends baseClass {
 		Thread.sleep(1000);
 		Enter_Password.sendKeys(pwd);
 		clicksignIn_Button.click();
-		CaptureScreen();
+		// CaptureScreen();
 
 	}
-public void CaptureScreen(){
 
-	try {
-		
-		File file=new File("/home/SImran/Documents/workspace-wspip/SeleniumWebdriver/src/test/java/error.jpg");
-		File source= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	public void ClickOnInbox() throws InterruptedException {
+		Thread.sleep(2000);
 
-FileUtils.copyFile(source, file);
-		
-	} catch (Exception e) {
-		e.printStackTrace();
+		Click_Inbox.click();
 	}
-	
-	
-	
-	
-	
-	
-	
-}
+
+	public void SignOut() throws InterruptedException {
+		Thread.sleep(2000);
+		Signout_link.click();
+		signout_button.click();
+	}
+
+	public void TotalNumberOfEmails() throws InterruptedException {
+		Thread.sleep(2000);
+		ClickOnInbox();
+		Thread.sleep(2000);
+		System.out.println(Click_Inbox.getText());
+
+	}
+
+	public void clickOnstarredemails() {
+		
+		clickOn_StarredEmails.click();
+	}
+	public void NoStarredEmails() throws InterruptedException{
+		Thread.sleep(2000);
+		clickOn_StarredEmails.click();
+		Thread.sleep(2000);
+		 Assert.assertTrue(driver.getPageSource().contains("VerifyNoStarredEmails"));
+	}
+
+	public void ClickOnStarInEmais() throws InterruptedException {
+		Thread.sleep(2000);
+		Click_Inbox.click();
+		Thread.sleep(2000);
+		clickon_Star.click();
+		Thread.sleep(2000);
+		clickOn_StarredEmails.click();
+		Thread.sleep(2000);
+	}
+
+	public void CaptureScreen() {
+
+		try {
+
+			File file = new File("/home/SImran/Documents/workspace-wspip/SeleniumWebdriver/src/test/java/error.jpg");
+			File source = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+			FileUtils.copyFile(source, file);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 }
